db3885f67ee0f1e10c8d8f5bc0b11eef0a52dcd0
Dheeraj Kumar Joshi <joshidj.12@gmail.com>
2017-05-05 15:57:53 +0530

    Update list-bucket.py

--- list-bucket.py
+++ list-bucket.py
@@ -3,7 +3,7 @@
 import boto3
 
 # let's use amazon s3
-s3 = boto3.resource('s3', aws_access_key_id = 'AKIAI6KLHZTL7BAVG6HA', aws_secret_access_key = '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')
+s3 = boto3.resource('s3', aws_access_key_id = 'IAI6KLHZTL7BAVG6', aws_secret_access_key = 's9fkJEgvm2+koAHFkz2d8/xOzR8/5tlZAp9To')
 print(s3)
 
 # for print out all the bucket names
