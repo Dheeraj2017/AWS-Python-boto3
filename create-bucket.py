a581e8a46014642cb0adf66fe4d7b73a5c53bb54
Dheeraj Kumar Joshi <joshidj.12@gmail.com>
2017-05-05 16:00:10 +0530

    Update create-bucket.py

--- create-bucket.py
+++ create-bucket.py
@@ -6,3 +6,19 @@ s3 = boto3.client('s3', aws_access_key_id = 'AI6KLHZTL7BAVG6', aws_secret_access
 
 #create bucket with unique name
 s3.create_bucket(Bucket='clinmdmcelebal1')
+
+#OUTPUT
+"""
+{'Location': '/clinmdmcelebal1',
+ 'ResponseMetadata': {'HTTPHeaders': {'content-length': '0',
+   'date': 'Fri, 05 May 2017 06:00:36 GMT',
+   'location': '/clinmdmcelebal1',
+   'server': 'AmazonS3',
+   'x-amz-id-2': 'Yuo3bdsJW4+02S0WJV5ZcZbXMNf191D+fKX/20JK8JRkYCnpiSXUXjespZtF5dyIn5yHyJg/WZ8=',
+   'x-amz-request-id': '8E52740B81017E22'},
+  'HTTPStatusCode': 200,
+  'HostId': 'Yuo3bdsJW4+02S0WJV5ZcZbXMNf191D+fKX/20JK8JRkYCnpiSXUXjespZtF5dyIn5yHyJg/WZ8=',
+  'RequestId': '8E52740B81017E22',
+  'RetryAttempts': 0}}
+
+"""
